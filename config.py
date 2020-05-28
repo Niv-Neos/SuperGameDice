0a517e5a291a82075a120c5c70b8aed86cb8bdd3
Niv-Neos <54379294+Niv-Neos@users.noreply.github.com>
2020-05-28 13:52:13 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 '''
 Ideal use 16:9 or 16:10 ratios smaller than your computer's resolution
-such as "1920x1080" or "1280x720"
+such as '1920x1080' or '1280x720'
 '''
 
 Resolution = '1280x720'
+Base_Font = 'system'
+Counter_Font = 'Arial 72'
+Modifier_Font = 'Arial 20'
