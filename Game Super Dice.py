886324e4fc8a37f2aa8f36e0948238e747224a71
Niv-Neos <54379294+Niv-Neos@users.noreply.github.com>
2020-05-30 13:41:50 -0400

    Add files via upload

--- Game Super Dice.py
+++ Game Super Dice.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import config as cf
 import random as rd
+import winsound as ws
 
 try:
     # Python2
@@ -21,6 +22,8 @@ main.clipboard_get
 
 number_i = tk.IntVar()
 number_ii = tk.IntVar()
+number_iii = tk.IntVar()
+number_iv = tk.IntVar()
 
 ### Rollers
 def Roll_Dice(quanity, sides):
@@ -37,7 +40,76 @@ def Roll_Dice(quanity, sides):
               log.insert(tk.INSERT, "["+str(a)+"]")
               log.config(state="disabled")
               number_ii.set(0)
+              number_iii.set(0)
+              number_iv.set(0)
               return a
+       while quanity == -1 and sides == -1:
+              q = number_iii.get()
+              s = number_iv.get()
+              quanity == q
+              sides = s
+              while steps < q:
+                  a = a + rd.randint(1,sides)
+                  steps = steps + 1
+                  while steps == q:
+                      mod = number_ii.get()
+                      a = a + mod
+                      number_i.set(a)
+                      log.config(state="normal")
+                      log.insert(tk.INSERT, "["+str(a)+"]")
+                      log.config(state="disabled")
+                      number_ii.set(0)
+                      number_iii.set(0)
+                      number_iv.set(0)
+                      return a
+
+def Determined_Direction():
+       steps = 0
+       a = 0
+       quanity = 1
+       while steps < quanity:
+              a = a + rd.randint(1,360)
+              steps = steps + 1
+       while steps == quanity:
+              number_i.set(a)
+              if cf.Direction_Output is 'degrees':
+                  log.config(state="normal")
+                  log.insert(tk.INSERT, "["+str(a)+"]")
+                  log.config(state="disabled")
+                  return a
+              elif cf.Direction_Output is 'radians':
+                  b = (a*mt.pi)/180
+                  log.config(state="normal")
+                  log.insert(tk.INSERT, "["+str(round(b,1))+"]")
+                  log.config(state="disabled")
+                  return a
+              elif cf.Direction_Output is 'compass':
+                  compass = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+                  b = (a/45)-1
+                  log.config(state="normal")
+                  log.insert(tk.INSERT, "["+compass[int(round(b,0))]+"]")
+                  log.config(state="disabled")
+                  return a
+              elif cf.Direction_Output is 'advance_compass':
+                  compass = ["N", "NNE", "NE", "NEE", "E", "SEE", "SE", "SSE", "S", "SSW", "SW", "SWW", "W", "NWW", "NW", "NNW"]
+                  b = (a/22.5)-1
+                  log.config(state="normal")
+                  log.insert(tk.INSERT, "["+compass[int(round(b,0))]+"]")
+                  log.config(state="disabled")
+                  return a
+              elif cf.Direction_Output is 'simple_compass':
+                  compass = ["N", "E", "S", "W"]
+                  b = (a/90)-1
+                  log.config(state="normal")
+                  log.insert(tk.INSERT, "["+compass[int(round(b,0))]+"]")
+                  log.config(state="disabled")
+                  return a
+              else:
+                  ws.PlaySound('SystemExclamation',1)
+                  log.config(state="normal")
+                  log.insert(tk.INSERT, "[Error, you need the correct direction string. Check config]")
+                  log.config(state="disabled")
+                  return a
 
 def Clear_Log():
         log.config(state="normal")
@@ -49,6 +121,18 @@ def Modifier(num):
         b = num + get
         number_ii.set(b)
         return b
+
+def Set_Custom_Quanity(num):
+        a = number_iii.get()
+        q = num + a
+        number_iii.set(q)
+        return q
+    
+def Set_Custom_Sides(num):
+        a = number_iv.get()
+        s = num + a
+        number_iv.set(s)
+        return s
             
 ### Dices
 d2b = tk.Button(main, text = ("d2  "), font = (cf.Base_Font),
@@ -142,8 +226,20 @@ d20b5 = tk.Button(main, text = ("5d20"), font = (cf.Base_Font),
 d20b6 = tk.Button(main, text = ("6d20"), font = (cf.Base_Font),
             command = lambda : Roll_Dice(6,20), fg = "blue violet", bg = "gray3").grid(row=7,column=7,ipadx=30,ipady=5)
 
+dc = tk.Button(main, text = ("Quanity +1"), font = (cf.Base_Font),
+            command = lambda : Set_Custom_Quanity(1), fg = "gray99", bg = "gray3").grid(row=8,column=3,ipadx=11,ipady=5)
+bc = tk.Button(main, text = ("Sides +1"), font = (cf.Base_Font),
+            command = lambda : Set_Custom_Sides(1), fg = "gray99", bg = "gray3").grid(row=8,column=4,ipadx=17,ipady=5)
+roll = tk.Button(main, text = ("Roll Custom"), font = (cf.Base_Font),
+            command = lambda : Roll_Dice(-1,-1), fg = "gray99", bg = "gray3").grid(row=8,column=2,ipadx=2,ipady=5)
+
+direction = tk.Button(main, text = ("┼"), font = (cf.Base_Font),
+            command = lambda : Determined_Direction(), fg = "gray99", bg = "gray3").grid(row=8,column=6,ipadx=39,ipady=5)
+percentage = tk.Button(main, text = ("%"), font = (cf.Base_Font),
+            command = lambda : Roll_Dice(1,100), fg = "gray99", bg = "gray3").grid(row=8,column=7,ipadx=40,ipady=5)
+
 clear_button = tk.Button(main, text = ("Clear Log"), font = (cf.Base_Font),
-            command = lambda : Clear_Log(), fg = "red4", bg = "brown1").grid(row=1,column=1,ipadx=30,ipady=5)
+            command = lambda : Clear_Log(), fg = "gray1", bg = "brown1").grid(row=1,column=1,ipadx=30,ipady=5)
 
 ### Modifier Addition
 plus_1 = tk.Button(main, text = ("+1  "), font = (cf.Base_Font),
@@ -152,7 +248,7 @@ plus_2 = tk.Button(main, text = ("+2  "), font = (cf.Base_Font),
             command = lambda : Modifier(2), fg = "gray1", bg = "CadetBlue1").grid(row=10,column=2,ipadx=30,ipady=5)
 plus_5 = tk.Button(main, text = ("+5  "), font = (cf.Base_Font),
             command = lambda : Modifier(5), fg = "gray1", bg = "CadetBlue1").grid(row=11,column=2,ipadx=30,ipady=5)
-plus_10 = tk.Button(main, text = ("+10 "), font = (cf.Base_Font),
+plus_10 = tk.Button(main, text = ("+10"), font = (cf.Base_Font),
             command = lambda : Modifier(10), fg = "gray1", bg = "CadetBlue1").grid(row=12,column=2,ipadx=30,ipady=5)
 
 minus_1 = tk.Button(main, text = ("-1  "), font = (cf.Base_Font),
@@ -161,7 +257,7 @@ minus_2 = tk.Button(main, text = ("-2  "), font = (cf.Base_Font),
             command = lambda : Modifier(-2), fg = "gray1", bg = "IndianRed1").grid(row=10,column=3,ipadx=30,ipady=5)
 minus_5 = tk.Button(main, text = ("-5  "), font = (cf.Base_Font),
             command = lambda : Modifier(-5), fg = "gray1", bg = "IndianRed1").grid(row=11,column=3,ipadx=30,ipady=5)
-minus_10 = tk.Button(main, text = ("-10 "), font = (cf.Base_Font),
+minus_10 = tk.Button(main, text = ("-10"), font = (cf.Base_Font),
             command = lambda : Modifier(-10), fg = "gray1", bg = "IndianRed1").grid(row=12,column=3,ipadx=30,ipady=5)
 
 ### Numbers and Processe
@@ -170,6 +266,12 @@ counter = tk.Label(main, textvariable = number_i, font = (cf.Counter_Font),
 
 modifier = tk.Label(main, textvariable = number_ii, font = (cf.Modifier_Font),
               fg = "gray99", bg = "Darkorchid4").grid(row=9,column=1,ipadx=150)
+
+custom_quanities = tk.Label(main, textvariable = number_iii, font = (cf.Modifier_Font),
+              fg = "gray99", bg = "RoyalBlue2").grid(row=8,column=0,ipadx=1)
+
+custom_sides = tk.Label(main, textvariable = number_iv, font = (cf.Modifier_Font),
+              fg = "gray99", bg = "Brown2").grid(row=8,column=1,ipadx=150)
 
 log = tk.Text(main, height=7, width=18, bg = "dark slate gray", fg = "SeaGreen1")
 log.grid(row=0,column=1,ipadx=100)
--- config.py
+++ config.py
@@ -7,3 +7,12 @@ Resolution = '1280x720'
 Base_Font = 'system'
 Counter_Font = 'Arial 72'
 Modifier_Font = 'Arial 20'
+
+Direction_Output = 'compass'
+'''
+'compass' (Directions under a compass)
+'advance_compass' (With additional, more precise directions)
+'simple_compass' (Only with N, E, S, W)
+'degress' (In degrees)
+'radians' (In radians)
+'''
