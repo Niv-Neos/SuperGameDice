f3e0b830b9f7daae45006a3f75c36f94d9e5020f
Niv-Neos <54379294+Niv-Neos@users.noreply.github.com>
2023-12-18 22:47:10 -0400

    Update sound_database.py

--- sound_database.py
+++ sound_database.py
@@ -9,8 +9,6 @@ Failure
 ''' 
 
 Default_i = [
-    ['D:/Users/Frank/Desktop/Science/Python Projects/Game Super Dice/Default_i/select_100-0.wav'],
-    ['D:/Users/Frank/Desktop/Science/Python Projects/Game Super Dice/Default_i/select_100-5.wav'],
     ['N'],
     ['N']
     ]
